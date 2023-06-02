52842d2b54a81d1c467d5d6d997545545d6737b7
Antwan <94127380+Antwannnn@users.noreply.github.com>
2023-06-02 08:33:52 +0200

    first commit

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,9 +1,48 @@
 package exercice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercice {
     public static List<String> solution(String str, List<Character> ordre) {
-        throw new UnsupportedOperationException("Unimplemented");
+        ArrayList<String> solution = new ArrayList<>();
+        if (!str.isEmpty()) {
+            List<String> stringToWordList = new ArrayList<>();
+            String current = "";
+            for (char c : str.toCharArray()) {
+                if (c == ' ' || c == '\'') {
+                    stringToWordList.add(current);
+                    current = "";
+                } else {
+                    if (Character.isAlphabetic(c) || Character.isDigit(c))
+                        current += c;
+                }
+            }
+            stringToWordList.add(current);
+            List<String> unsortedWords = new ArrayList<>();
+            for(String s : stringToWordList){
+                unsortedWords.add(s);
+            }
+            System.out.println(stringToWordList);
+            for (char c : ordre) {
+                for (String s : stringToWordList) {
+                    if (Character.toLowerCase(s.toCharArray()[0]) == Character.toLowerCase(c)) {
+                        solution.add(s);
+                        unsortedWords.remove(s);
+                    }
+                }
+            }
+            for(String s : unsortedWords){
+                solution.add(s);
+            }
+        } else{
+            solution.add("");
+        }
+        return solution;
     }
+
+    /*
+    FEUJ GAY FADE
+    FHJGD
+     */
 }
--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -4,9 +4,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String texte = "exemple de texte a classer";
+        long startTime = System.nanoTime();
+        String texte = "Exemple de texte a classer";
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
         System.out.println(Exercice.solution(texte,ordre));
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        double durationMillis = (double) duration / 1_000_000.0;
+        System.out.println("Temps d'exécution : " + durationMillis);
         // output: [classer, texte, exemple, a, de]
     }
 }
