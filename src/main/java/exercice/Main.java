ed1e7048c19c07fae05060afde7d5be970d7383c
Antwan <94127380+Antwannnn@users.noreply.github.com>
2023-06-16 16:20:37 +0200

    final commits among final commit that are among the final commits

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -5,11 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         String texte = "Exemple de texte a classer";
-        ResourceRecorder r = new ResourceRecorder();
-        r.start();
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
-        r.stop();
-        System.out.println(r);
         System.out.println(Exercice.solution(texte,ordre));
         // output: [classer, texte, exemple, a, de]
     }
