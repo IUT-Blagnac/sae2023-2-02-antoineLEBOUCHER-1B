d446ab86093e1d2f87f496e55084e4d232a17723
Antwan <94127380+Antwannnn@users.noreply.github.com>
2023-06-16 16:31:27 +0200

    utilisation resource recorder

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -4,9 +4,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        ResourceRecorder resourceRecorder = new ResourceRecorder();
         String texte = "Exemple de texte a classer";
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
+        resourceRecorder.start();
         System.out.println(Exercice.solution(texte,ordre));
+        resourceRecorder.stop();
+        System.out.print(resourceRecorder.toString());
         // output: [classer, texte, exemple, a, de]
     }
 }
